4c6f3913d4067546ab06c30fdfe344694ec7d0da
Mark Jenkins <ma.jenkins@uwinnipeg.ca>
2021-01-27 10:37:31 -0600

    fix failure to adjust the app name in jeromeneedsill.illrequest in last commit 21014c990d7b069c8d5ce9477e511e46d7ad7073

--- jeromeneedsill/illrequest/apps.py
+++ jeromeneedsill/illrequest/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class IllrequestConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'illrequest'
+    name = 'jeromeneedsill.illrequest'
