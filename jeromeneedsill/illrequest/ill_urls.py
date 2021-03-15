14db97850a65d8416ce52bd008c5550cea67047d
Mark Jenkins <ma.jenkins@uwinnipeg.ca>
2021-03-15 18:05:52 -0500

    indentation fix

--- jeromeneedsill/illrequest/ill_urls.py
+++ jeromeneedsill/illrequest/ill_urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import openurl_requestlog_and_directtologin, post_oauth
 
 urlpatterns = [
-     path('requestlogin', openurl_requestlog_and_directtologin,
+    path('requestlogin', openurl_requestlog_and_directtologin,
          name='requestlogin'),
     path('PostOAuth', post_oauth),
 ]
